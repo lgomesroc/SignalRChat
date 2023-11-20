ee602b309ef685b9eadcaf02e0c7399aec72a24a
Luciano Gomes Rocha <]'lgomesroc@hotmail.com>
2023-11-20 14:15:39 -0300

    Programa de Chat com o .NET e C# junto com o SignalR

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message) => await Clients.All.SendAsync("ReceiveMessage", user, message);
+        public async Task SendMessageToCaller(string user, string message) => await Clients.Caller.SendAsync("ReceiveMessage", user, message);
+        public async Task SendMessageToGroup(string user, string message) => await Clients.Group("SignalR Users").SendAsync("ReceiveMessage", user, message);
+    }
+}
